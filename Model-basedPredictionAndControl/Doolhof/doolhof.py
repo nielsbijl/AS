ac8d25fd9320dfb9dc1a087ff931213a5d64b067
Niels <nbijl@petersplanet.nl>
2021-11-17 13:29:40 +0100

    step function

--- Model-basedPredictionAndControl/Doolhof/doolhof.py
+++ Model-basedPredictionAndControl/Doolhof/doolhof.py
@@ -17,7 +17,13 @@ class doolhof:
         if action == 0:  # move up
             newPos = (state.position[0], state.position[1] + 1)
             newSate = state(newPos, )
-        pass
+            pass
+        elif action == 1:  # move right
+            pass
+        elif action == 2:  # move down
+            pass
+        else:  # move left
+            pass
 
     def __str__(self):
         return "map: %s \n rewards: %s \n state: %s" % (self.map, self.rewards, self.state)
