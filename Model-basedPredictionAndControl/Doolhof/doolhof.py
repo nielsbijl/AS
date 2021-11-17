b4ad7f121ad6ecd200a277b01b59cbf3ab623307
Niels <nbijl@petersplanet.nl>
2021-11-17 13:28:01 +0100

    doolhof class

---
+++ Model-basedPredictionAndControl/Doolhof/doolhof.py
@@ -0,0 +1,23 @@
+from state import state
+
+
+class doolhof:
+    def __init__(self, state: state):
+        self.map = [[0, 0, 0, 0],
+                    [0, 0, 0, 0],
+                    [0, 0, 0, 0],
+                    [0, 0, 0, 0]]
+        self.rewards = [[0, 0, 0, 0],
+                        [0, 0, 0, 0],
+                        [0, 0, 0, 0],
+                        [0, 0, 0, 0]]
+        self.state = state
+
+    def step(self, state: state, action: int) -> state:
+        if action == 0:  # move up
+            newPos = (state.position[0], state.position[1] + 1)
+            newSate = state(newPos, )
+        pass
+
+    def __str__(self):
+        return "map: %s \n rewards: %s \n state: %s" % (self.map, self.rewards, self.state)
