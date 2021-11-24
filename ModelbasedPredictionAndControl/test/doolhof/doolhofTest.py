f5cd650d71422bd57f7c92c9fce04953c52a7cd3
Niels <nbijl@petersplanet.nl>
2021-11-24 14:34:11 +0100

    unit tests added

--- ModelbasedPredictionAndControl/test/doolhof/doolhofTest.py
+++ ModelbasedPredictionAndControl/test/doolhof/doolhofTest.py
@@ -6,38 +6,36 @@ from ModelbasedPredictionAndControl.doolhof.policy import policy
 
 
 class agentTest(unittest.TestCase):
-    def testValueIteration(self):
-        expected = [[38, 39, 40, 0],
-                    [37, 38, 39, 40],
-                    [36, 37, 36, 35],
-                    [0, 36, 35, 34]]
+    def testRun(self):
+        expected = [(2, 0), (2, 1), (1, 1), (1, 2), (1, 3), (2, 3), (3, 3)]
 
-        startPos = (2, 0)
-        startReward = 0
-
+        startPos, startReward = (2, 0), 0
         initState = state(startPos, startReward, False)
 
         maze = doolhof(initState)
         pol = policy(maze)
-
         player = agent(maze, pol, startPos)
 
-        result = player.valueIteration(discount=1, threshhold=0.1)
+        result = maze.run(agent=player, deterministic=True)
 
         self.assertEqual(expected, result)
 
-    def testValueCalcuation(self):
-        startPos = (2, 0)
-        startReward = 0
-
+    def testCoordsToIndex(self):
+        startPos, startReward = (2, 0), 0
         initState = state(startPos, startReward, False)
 
         maze = doolhof(initState)
-        pol = policy(maze)
 
-        player = agent(maze, pol, startPos)
+        self.assertEqual((3, 0), maze.coordsToIndex((0, 0)))
 
-        self.assertEqual(-1, player.valueCalculate(location=startPos, discount=1))
+
+    def testIndexToCoords(self):
+        startPos, startReward = (2, 0), 0
+        initState = state(startPos, startReward, False)
+
+        maze = doolhof(initState)
+
+        self.assertEqual((0, 0), maze.indexToCoords((3, 0)))
 
 
 if __name__ == '__main__':
